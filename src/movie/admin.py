5899aa942bb97f49d6595d2837f3f28d2d01fd09
Arjunsingh <arjun.yadav015@gmail.com>
2017-08-15 17:19:49 +0530

    done

--- src/movie/admin.py
+++ src/movie/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
 from .models import (Movie, Genre)
 
+
 class GenreAdmin(admin.ModelAdmin):
     """
     Genre admin
     """
     pass
 
+
 class MovieAdmin(admin.ModelAdmin):
     """
     Movie admin
     """
     pass
-
-
 
 
 admin.site.register(Genre, GenreAdmin)
--- src/movie/views.py
+++ src/movie/views.py
@@ -1,11 +1,23 @@
 from rest_framework.views import APIView
 from rest_framework.settings import api_settings
-from rest_framework.permissions import (IsAuthenticated, )
+from rest_framework.permissions import (IsAuthenticated, BasePermission)
 from rest_framework import viewsets
-#from rest_framework.response import Response  
+#from rest_framework.response import Response
 from django.http import HttpResponse
 from .models import (Movie, Genre)
 from .serializers import (MovieSerializer, GenreSerializer)
+
+
+class AdminUserPermissions(BasePermission):
+    """
+    Allow only the Admin to have access to all methods and non-admin has access to only GET method
+    """
+
+    def has_permission(self, request, view):
+        if not request.user.is_superuser:
+            if not request.method == 'GET':
+                return False
+        return True
 
 
 class GenreViewSet(viewsets.ModelViewSet):
@@ -13,8 +25,8 @@ class GenreViewSet(viewsets.ModelViewSet):
     Genre ViewSet for all genres
     """
     authentication_classes = api_settings.DEFAULT_AUTHENTICATION_CLASSES
-    permission_classes = (IsAuthenticated, )
-    serializer_class = GenreSerializer 
+    permission_classes = (IsAuthenticated, AdminUserPermissions)
+    serializer_class = GenreSerializer
 
     def get_queryset(self):
         return Genre.objects.all()
@@ -22,11 +34,11 @@ class GenreViewSet(viewsets.ModelViewSet):
 
 class MovieViewSet(viewsets.ModelViewSet):
     """
-    Movie View to return all movies
+    Movie View to create, update, delete movies
     """
-    #authentication_classes = api_settings.DEFAULT_AUTHENTICATION_CLASSES
-    #permission_classes = (IsAuthenticated, )
-    serializer_class = MovieSerializer 
+    authentication_classes = api_settings.DEFAULT_AUTHENTICATION_CLASSES
+    permission_classes = (IsAuthenticated, AdminUserPermissions)
+    serializer_class = MovieSerializer
 
     def get_queryset(self):
         return Movie.objects.prefetch_related('genres').all()
